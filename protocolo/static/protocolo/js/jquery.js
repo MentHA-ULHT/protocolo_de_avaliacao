33c9c901b1edca8cb8c4d54317c71256e3b2704c
Nuno Rasteiro <a21906959@alunos.ulht.pt>
2022-04-27 00:19:19 +0100

    Adicionei CSRF Token ao AJAX Instalei a biblioteca js.cookies

--- protocolo/static/protocolo/js/jquery.js
+++ protocolo/static/protocolo/js/jquery.js
@@ -3,9 +3,11 @@ $(document).ready(function (){
     $(document).on("click",".btn",function (){
             var id = $(this).attr("id"); //Não é usado para nada, por agora
             var href = $(this).attr("data-href");
+            const csrf_token = Cookies.get('csrftoken');
        $.ajax({
            method: 'GET',
            url: href,
+           headers:{'X-CSRFToken': csrf_token},
            success: function (data){
                console.log("Success!");
                $('.container').html(data);
